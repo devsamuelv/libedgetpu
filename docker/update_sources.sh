0f4700d5759d9318f98005cf0f7e3ad3b2c51353
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2025-02-25 00:50:21 -0500

    update

--- docker/update_sources.sh
+++ docker/update_sources.sh
@@ -12,14 +12,53 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME} main universe
+# deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-updates main universe
+# deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-security main universe
+
 . /etc/os-release
 
 [[ "${NAME}" == "Ubuntu" ]] || exit 0
 
-sed -i "s/deb\ /deb \[arch=amd64\]\ /g" /etc/apt/sources.list
+# sed -i "s/deb\ /deb \[arch=amd64\]\ /g" /etc/apt/sources.list.d/ubuntu.sources
 
-cat <<EOT >> /etc/apt/sources.list
-deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME} main universe
-deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-updates main universe
-deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-security main universe
-EOT
+# cat <<EOT >> /etc/apt/sources.list.d/ubuntu.sources
+# Types: deb
+# URIs: http://ports.ubuntu.com/ubuntu-ports
+# Suites: noble
+# Components: main universe
+# Architectures: arm64 armhf
+
+# Types: deb
+# URIs: http://ports.ubuntu.com/ubuntu-ports
+# Suites: noble-updates
+# Components: main universe
+# Architectures: arm64 armhf
+
+# Types: deb
+# URIs: http://ports.ubuntu.com/ubuntu-ports
+# Suites: noble-security
+# Components: main universe
+# Architectures: arm64 armhf
+# EOT
+
+echo "
+Types: deb
+URIs: http://ports.ubuntu.com/ubuntu-ports
+Suites: noble
+Components: main universe
+Architectures: arm64 armhf
+
+Types: deb
+URIs: http://ports.ubuntu.com/ubuntu-ports
+Suites: noble-updates
+Components: main universe
+Architectures: arm64 armhf
+
+Types: deb
+URIs: http://ports.ubuntu.com/ubuntu-ports
+Suites: noble-security
+Components: main universe
+Architectures: arm64 armhf
+" | cat > /etc/apt/sources.list.d/ubuntu.sources
